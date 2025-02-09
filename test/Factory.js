8e22c4b55d04c1cc1b5c7097e4cc2d37c3ad8e7b
Graham Kirwan <graham.kirwan4@gmail.com>
2025-02-09 17:27:36 -0500

    setup hardhat tests

--- test/Factory.js
+++ test/Factory.js
@@ -6,12 +6,15 @@ describe("Factory", function () {
     const FEE = ethers.parseUnits("0.01", 18)
 
     async function deployFactoryFixtures() {
+        // Fetch accounts
+        const [deployer] = await ethers.getSigners()
+
         // Fetch the contract
         const Factory = await ethers.getContractFactory("Factory")
         // Deploy the contract
         const factory = await Factory.deploy(FEE)
 
-        return { factory }
+        return { factory, deployer }
     }
 
     describe("Deployment", function() {
@@ -19,7 +22,14 @@ describe("Factory", function () {
             const { factory } = await loadFixture(deployFactoryFixtures)
             expect(await factory.fee()).to.equal(FEE)
         })
+
+        it("Should set the owner", async function(){
+            const { factory, deployer } = await loadFixture(deployFactoryFixtures)
+            expect(await factory.owner()).to.equal(deployer.address)
+        })
     })
+
+    
 
 //     it("should have a name", async function() {
 //         const { factory } = await deployFactoryFixtures()
