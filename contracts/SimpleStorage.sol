d8b1961d61025219e5b1ed1baf15d67c2ab0e941
Graham Kirwan <graham.kirwan4@gmail.com>
2025-02-11 21:53:00 -0500

    added simple storage contract

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19; // stating our version - compiler versions
+
+contract SimpleStorage {
+
+    uint256 myFavNum = 6; // state variable
+
+    // uint256[] lisOfFavNums;
+    
+    // Define a struct
+    struct Person {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // Define a dynamic array
+    Person[] public listOfPeople;
+
+    // Mapping (Better than an array since we can map content with a 'key' to easily find - Think a dictionary)
+    mapping (string => uint256) public nameToFavNumber;
+
+    // Storage function
+    function storeFavNum(uint256 _favNum) public {
+        myFavNum =  _favNum;
+    }
+
+    // Retreival function
+    function retreiveFavNum() public view returns(uint256) {
+        return myFavNum;
+    }
+
+    // Add a person of struct type Person to a dynamic array and add them to our mapping
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push(Person(_favouriteNumber, _name)); 
+        nameToFavNumber[_name] = _favouriteNumber;
+    }
+}
